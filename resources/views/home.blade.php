e7e6a512a872662bdcfa9272a7b0db3d2636f30f
magnoliaaz <magnoliaazizah@gmail.com>
2026-06-10 11:03:20 +0700

    .

---
+++ resources/views/home.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>RuangCraftku</title>
+
+    <!-- CSS Bootstrap/Template -->
+    <link href="{{ asset('assets/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/css/style.css') }}" rel="stylesheet">
+</head>
+<body>
+
+    <h1>Halo RuangCraftku</h1>
+
+    <!-- JS -->
+    <script src="{{ asset('assets/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('assets/js/main.js') }}"></script>
+
+</body>
+</html>
